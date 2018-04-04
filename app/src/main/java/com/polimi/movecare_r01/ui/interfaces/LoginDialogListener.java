f4fb00f007c063b6a4031e8c335fcda930e4aa6b
stefanotolomeo <ste.tolomeo@gmail.com>
2018-04-04 10:38:50 +0200

    LoginDialogListener

---
+++ app/src/main/java/com/polimi/movecare_r01/ui/interfaces/LoginDialogListener.java
@@ -0,0 +1,7 @@
+package com.polimi.movecare_r01.ui.interfaces;
+
+import android.support.v4.app.DialogFragment;
+
+public interface LoginDialogListener {
+    void onDialogPositiveClick(DialogFragment dialog);
+}
